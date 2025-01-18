6b667ff840587e7961187d287e4ce5c9b88a27f3
Faysel Abdella <fayselcode@gmail.com>
2025-01-18 21:33:36 +0300

    Add top navigation

---
+++ src/app/components/TopNav.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+import React from "react";
+
+const TopNav = () => {
+  return (
+    <section className="py-10 flex items-center justify-center">
+      <Image
+        src="/assets/our-logo.svg"
+        alt="Our company logo"
+        width={264}
+        height={43}
+      />
+    </section>
+  );
+};
+
+export default TopNav;
