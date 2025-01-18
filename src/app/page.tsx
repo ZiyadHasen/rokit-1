69a725c9e8f105d25c64dbeedaec891a0a71cf6c
Faysel Abdella <fayselcode@gmail.com>
2025-01-18 21:37:07 +0300

    Add demo image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
+import Image from "next/image";
 import TopNav from "./components/TopNav";
 
 export default function Home() {
   return (
     <section>
       <TopNav />
+      <section>
+        <Image
+          src="/assets/demo-cover.svg"
+          alt="cover"
+          width={1920}
+          height={678}
+          className="object-cover"
+        />
+      </section>
     </section>
   );
 }
