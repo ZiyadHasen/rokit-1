b8d8233cf46150544415f99569925d7cc29ba93c
Faysel Abdella <fayselcode@gmail.com>
2025-01-23 11:28:28 +0300

    Add the logic to save the user data to the database

--- src/pages/api/user.ts
+++ src/pages/api/user.ts
@@ -1,10 +1,45 @@
+import mysql from "mysql2/promise";
 import { NextApiRequest, NextApiResponse } from "next";
+
+let connectionParams = {
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "0966463034f",
+  database: "hdadmin",
+};
 
 export default async function saveUserData(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  if (req.method === "POST") {
-    console.log(req.body);
+  try {
+    const connection = mysql.createConnection(connectionParams);
+
+    if (req.method === "POST") {
+      const { ipAddress, country, region, city } = req.body;
+
+      (await connection).query(`CREATE TABLE IF NOT EXISTS UserData (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+          ipAddress VARCHAR(255),
+          country VARCHAR(255),
+          region VARCHAR(255),
+          cityName VARCHAR(255)
+        )`);
+
+      (await connection).query(
+        `
+        INSERT INTO UserData (ipAddress, country, region, cityName)
+        VALUES (?, ?, ?, ?);
+      `,
+        [ipAddress, country, region, city]
+      );
+
+      // console.log(req.body);
+
+      return res.status(200).json({ message: "This user data is saved" });
+    }
+  } catch (error) {
+    console.log(error);
   }
 }
