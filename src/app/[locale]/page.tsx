5403a21d5a5959f966a168ba27448cbbd552f65d
Faysel Abdella <fayselcode@gmail.com>
2025-01-23 10:07:59 +0300

    Add header and footer

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -2,7 +2,10 @@ import Image from "next/image";
 import TopNav from "@/components/TopNav";
 import Tabs from "@/components/Tabs";
 
+import { useTranslations } from "next-intl";
+
 export default function Home() {
+  const t = useTranslations("HomePage");
   return (
     <section>
       <TopNav />
@@ -16,12 +19,34 @@ export default function Home() {
           aria-label="Hero section image"
         />
       </section>
-      <section className="max-container px-4 mt-32 max-md:mt-12">
-        <h2 className="text-black font-bold text-5xl max-md:text-3xl mb-24 max-md:mb-12">
+      <section
+        className="max-container px-4 mt-16 max-md:mt-12 
+text-black"
+      >
+        <h1 className="font-bold text-4xl leading-10 ">
+          {t("header.mainHeader")}
+        </h1>
+        <h2 className="mt-1 font-bold text-2xl">{t("header.subHeader")}</h2>
+        <p className="font-normal text-2xl">{t("header.firstContent")}</p>
+        <p className="mt-2 font-normal text-2xl">{t("header.secondContent")}</p>
+        <p className="font-normal text-2xl">{t("header.thirdContent")}</p>
+        <p className="mt-2 font-normal text-2xl">{t("header.forthContent")}</p>
+
+        <p className="mt-14 font-bold text-5xl max-md:text-3xl mb-20 max-md:mb-12">
           eIFU Downloads
-        </h2>
+        </p>
         <Tabs />
       </section>
+
+      <footer className="text-black text-center mt-6 pb-12">
+        <p className="font-normal text-xl">{t("footer.info")}</p>
+        <p className="font-normal text-xl">
+          COPYRIGHT ⓒ{" "}
+          <span className="font-semibold">
+            ROKIT HEALTHCARE, INC. ALL RIGHTS RESERVED
+          </span>
+        </p>
+      </footer>
     </section>
   );
 }
